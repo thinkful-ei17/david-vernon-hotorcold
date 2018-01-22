a3bc5005db1019464fcc049e82dcb73fa4c613d3
David Park <dysp92@gmail.com>
2018-01-22 11:10:03 -0800

    whats going on

--- src/game.js
+++ src/game.js
@@ -1,8 +1,39 @@
 import React from 'react';
 
 export default function Game(props) {
+  const getInput = function(event) {
+    console.log(this.textInput.value());
+  }
+  const onSubmit = function(event) {
+    console.log('anything');
+    event.preventDefault();
+    const text = this.textInput.value.trim();
+    console.log(text);
+    // TODO: Add the card or list
+    this.textInput.value = '';
+  }
+
   return (
-    <button onClick={()=> props.printSomething()}>button</button>
+    <div className='game'>
+      <div className='hotOrCold'>
+        <p>test</p>
+      </div>
+      <form className="card add-form" onSubmit={(e) => onSubmit(e)}>
+          <input
+              type="text"
+              // ref={input => this.textInput = input}
+              // aria-label={label}
+          />
+          <button>Add</button>
+      </form>
+      {/* <button onClick={()=> getInput()}>Guess</button> */}
+      <div className='guessCount'>
+        <p>Guess#0</p>
+      </div>
+      <div className='arrayTiles'>
+
+      </div>
+    </div>
   )
 
 }
